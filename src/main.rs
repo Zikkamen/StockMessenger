d48315fe7f4a8fe4dad160ee341e8a6572d08a76
Zikkamen <jeffreyntue@gmail.com>
2024-09-26 17:19:27 +0200

    Removed static ip adress

--- src/main.rs
+++ src/main.rs
@@ -4,6 +4,6 @@ mod websockets;
 use crate::websockets::websocket_server::WebSocketServer;
 
 fn main() {
-    let websocket_server = WebSocketServer::new("localhost:9002", "152.53.36.150:9004");
+    let websocket_server = WebSocketServer::new("localhost:9002", "localhost:9004");
     websocket_server.start_server();
 }
